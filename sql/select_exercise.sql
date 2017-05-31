b54422776b41e6d64d414ec5830fd091e8577553
micahdisney <micah.n.disney@gmail.com>
2017-05-31 16:32:49 -0500

    corrected sytnax

--- sql/select_exercise.sql
+++ sql/select_exercise.sql
@@ -2,21 +2,34 @@ USE codeup_test_db;
 
 
 SELECT 'The name of all albums by Pink Floyd.' AS '';
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
 
 SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' as '';
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 SELECT 'The genre fom Nevermind' as '';
-SELECT  genre FROM albums WHERE name = 'Nevermind';
+SELECT  genre
+FROM albums
+WHERE name = 'Nevermind';
 
 SELECT 'Which albums were released in the 1990s' as '';
-SELECT  name FROM albums WHERE release_date between 1990 and 1999;
+SELECT  name
+FROM albums
+WHERE release_date between 1990 and 1999;
 
 SELECT 'Which albums had less than 20 million certified sales' as '';
-SELECT  name FROM albums WHERE sales < 20;
+SELECT  name
+FROM albums
+WHERE sales < 20;
 
 SELECT 'All the albums in the rock genre.' as '';
-SELECT genre FROM albums WHERE genre like '%Rock%';
+SELECT genre
+FROM albums
+WHERE genre like '%Rock%';
+
 
 
