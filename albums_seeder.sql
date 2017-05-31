552ccebd1ec13639d6f15d58fc0b1a1e86fa236b
micahdisney <micah.n.disney@gmail.com>
2017-05-31 15:00:42 -0500

    completed insert exercise part 1

---
+++ albums_seeder.sql
@@ -0,0 +1,31 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '46.3', 'Pop, Rock, R&B');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('AC/DC', 'Back in Black', '1980', '26.1', 'Hard Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.2', 'Progressive Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'R&B, Soul, Pop, Soundtrack');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Meat Loaf', 'Bat out of Hell', '1977', '21.5', 'Hard Rock, Progressive Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Their Greatest Hits (1971–1975)', '1976', '32.2', 'Rock, Soft rock, Folk rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Bee Gees', 'Saturday Night Fever', '1977', '20.6', 'Disco');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('FleetWood Mac', 'Rumours', '1977', '27.9', 'Soft Rock');
+
+
+
+
+
+
