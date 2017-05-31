54258ad71065366b9035830da54381619043b48c
micahdisney <micah.n.disney@gmail.com>
2017-05-31 11:41:00 -0500

    -completed tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+      CREATE TABLE albums (
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        artist VARCHAR(225),
+        name VARCHAR(225),
+        release_date INTEGER,
+        sales FLOAT,
+        genre LINESTRING
+      );
