aa43be778ea94bbb393e9f1d56fef92e4e87ef91
micahdisney <micah.n.disney@gmail.com>
2017-05-31 10:50:31 -0500

    -added .gitignore

--- tables-lesson.sql
+++ tables-lesson.sql
@@ -1,1 +1,11 @@
       -- Tables Lesson
+
+      select * from mysql.user\G
+
+
+      CREATE TABLE quote (
+        id INT UNSIGNED NOT NULL AUTO_INCREMNET,
+        author_code CHAR(3),
+
+
+      );
