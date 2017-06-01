9713ff6da748b7437d9ea0f485c06b2df13867f5
micahdisney <micah.n.disney@gmail.com>
2017-06-01 15:36:30 -0500

    completed where continued exercise

--- sql/where_exercises.sql
+++ sql/where_exercises.sql
@@ -1,21 +1,54 @@
 
 
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+# SELECT emp_no, first_name, last_name
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+#
+# SELECT emp_no, first_name, last_name
+# FROM employees
+# WHERE last_name LIKE 'E%';
+#
+# SELECT emp_no, first_name, last_name, hire_date
+# FROM employees
+# WHERE hire_date BETWEEN '1990-01-01' AND  '1999-12-31';
+#
+# SELECT emp_no, first_name, last_name, birth_date
+# FROM employees
+# WHERE birth_date LIKE '%12-25';
+#
+# SELECT emp_no, first_name, last_name
+# FROM employees
+# WHERE last_name LIKE '%q%';
+
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya';
+
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
+AND gender = 'M';
+
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
 
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND  '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND  '1999-12-31'
+AND birth_date LIKE '%12-25';
 
-SELECT emp_no, first_name, last_name, birth_date
-FROM employees
-WHERE birth_date LIKE '%12-25';
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND NOT '%qu%';
+
