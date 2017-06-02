01a375798893204300bb8b01c8686d3ef53eaf79
micahdisney <micah.n.disney@gmail.com>
2017-06-02 11:16:54 -0500

    -Opedn the order_by_exercises.sql script and saved it as functions_exercises.sql

---
+++ sql/functions_exercises.sql
@@ -0,0 +1,51 @@
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE first_name = 'Irena'
+      OR first_name = 'Vidya'
+      OR first_name = 'Maya'
+ORDER BY last_name DESC, first_name DESC;
+
+
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE (first_name = 'Irena'
+       OR first_name = 'Vidya'
+       OR first_name = 'Maya')
+      AND gender = 'M';
+
+
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E'
+ORDER BY emp_no DESC ;
+
+
+SELECT
+  emp_no,
+  first_name,
+  last_name,
+  hire_date,
+  birth_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+
+SELECT
+  emp_no,
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND NOT '%qu%';
